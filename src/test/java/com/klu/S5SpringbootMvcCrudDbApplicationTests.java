96c1ea93e75207c3bb70e42de62eb655d24c58e3
kl2400030727 <snehikurapati@gmail.com>
2026-03-31 09:23:50 +0530

    Added Project

---
+++ src/test/java/com/klu/S5SpringbootMvcCrudDbApplicationTests.java
@@ -0,0 +1,12 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5SpringbootMvcCrudDbApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5SpringbootMvcCrudDbApplicationTests.class, args);
+    }
+}
