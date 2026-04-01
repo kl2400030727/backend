d41f7390d86f249093fba96d57dbd5f73b03b99e
kl2400030727 <snehikurapati@gmail.com>
2026-04-01 10:15:44 +0530

    Updated

---
+++ src/main/java/com/klu/S5SpringbootMvcCrudDbApplication.java
@@ -0,0 +1,12 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5SpringbootMvcCrudDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5SpringbootMvcCrudDbApplication.class, args);
+    }
+}
